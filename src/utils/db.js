1496717057cee457f1e99dd55266da26eda3dbc7
Thalis Antunes <thalisantunessilva@gmail.com>
2025-05-29 13:51:12 -0300

    fix: repair env

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,26 +1,29 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.PGUSER, 
-  process.env.PGPASSWORD, 
+  process.env.DB_NAME,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  process.env.PGDATABASE,
   {
-  host: process.env.PGHOST,
-  port: 5432,
-  dialect: 'postgres',
-  ssl: {
-    rejectUnauthorized: false
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: console.log,
   }
-});
+);
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+    console.log("Conexão com o banco de dados estabelecida com sucesso!");
   })
   .catch((error) => {
-    console.error('Erro ao conectar com o banco de dados:', error);
+    console.error("Erro ao conectar com o banco de dados:", error);
   });
 
 module.exports = sequelize;
